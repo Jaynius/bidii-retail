a482d7f6753cc5343ee1cdc9e0d611708cff6326
jaynius <bramjaynius@gmail.com>
2023-10-28 08:04:15 +0300

    bootstrap project

---
+++ src/main/java/com/jaynius/bidiiretail/BidiiRetailApplication.java
@@ -0,0 +1,13 @@
+package com.jaynius.bidiiretail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidiiRetailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidiiRetailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jaynius/bidiiretail/BidiiRetailApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jaynius.bidiiretail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidiiRetailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
